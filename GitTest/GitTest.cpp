3ed00753f7b142e82865bfbc2162e9dc1a6de11c
健太 萩原 <kenhagi1316@outlook.com>
2023-10-11 14:34:22 +0900

    日付の出力

--- GitTest/GitTest.cpp
+++ GitTest/GitTest.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello Github\n";
 
     std::cout << "LC1D_21_ハギワラ_ケンタ\n";
+
+    std::cout << "2023/10/11\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
